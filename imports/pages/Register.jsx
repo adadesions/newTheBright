462f0fd164fd90bb5780cb484108870c48afbb37
adadesions <ada_dev@icloud.com>
2016-06-02 16:21:58 +0700

    Add ref attr to register form

--- imports/pages/Register.jsx
+++ imports/pages/Register.jsx
@@ -51,7 +51,7 @@ export default class Register extends React.Component {
           <div className="col s10 l8 form-section">
             <div className="col s12 l12 head-form">ลงทะเบียนเรียน</div>
             <div className="input-field col s12 l12">
-              <select defaultValue="0">
+              <select ref="course" defaultValue="0">
                 <option value="0" disabled>Choose your option</option>
                 <option value="1">เดอะไบร์ท ติว ติด มันส์ พลัส - หาดใหญ่</option>
                 <option value="2">เดอะไบร์ท ติว ติด มันส์ พลัส - ขอนแก่น</option>
@@ -60,71 +60,71 @@ export default class Register extends React.Component {
               <label>คอร์สเรียน</label>
             </div>
             <div className="input-field col s4 l2">
-              <select defaultValue="0">
+              <select ref="prefix" defaultValue="0">
                 <option value="0" disabled>Choose</option>
-                <option value="m">นาย</option>
-                <option value="f">นางสาว</option>
+                <option value="Mr">นาย</option>
+                <option value="Ms">นางสาว</option>
               </select>
               <label>คำนำหน้า</label>
             </div>
             <div className="input-field col s8 l6">
               <input id="name" type="text" className="validate"/>
-              <label for="name">ชื่อ-นามสกุล</label>
+              <label ref="fullname" for="name">ชื่อ-นามสกุล</label>
             </div>
             <div className="input-field col s12 l4">
-              <input id="pin-code" type="number" className="validate"/>
-              <label for="pin-code">เลขประจำตัวประชาชน</label>
+              <input ref="id-number" id="id-number" type="number" className="validate"/>
+              <label for="id-number">เลขประจำตัวประชาชน</label>
             </div>
             <div className="input-field col s12 l6">
-              <input id="nick-name" type="text" className="validate"/>
+              <input ref="nick-name" id="nick-name" type="text" className="validate"/>
               <label for="nick-name">ชื่อเล่น</label>
             </div>
             <div className="input-field col s12 l6">
-              <input id="birthday" type="date" className="datepicker"/>
+              <input ref="birthday" id="birthday" type="date" className="datepicker"/>
               <label for="birthday">วันเกิด</label>
             </div>
             <div className="input-field col s12 l12">
-              <input id="school-name" type="text" className="validate"/>
+              <input ref="school-name" id="school-name" type="text" className="validate"/>
               <label for="school-name">โรงเรียน</label>
             </div>
             <div className="input-field col s6 l6">
-              <input id="class" type="text" className="validate"/>
+              <input ref="class" id="class" type="text" className="validate"/>
               <label for="class">ชั้นปี</label>
             </div>
             <div className="input-field col s6 l6">
-              <input id="program" type="text" className="validate"/>
+              <input ref="program" id="program" type="text" className="validate"/>
               <label for="program">สาย</label>
             </div>
             <div className="input-field col s12 l12">
-              <textarea id="address" className="materialize-textarea"></textarea>
+              <textarea ref="address" id="address" className="materialize-textarea"></textarea>
               <label for="address">ที่อยู่</label>
             </div>
             <div className="input-field col s12 l6">
-              <input id="tel" type="text" className="validate"/>
+              <input ref="tel" id="tel" type="text" className="validate"/>
               <label for="tel">เบอร์โทร</label>
             </div>
             <div className="input-field col s12 l6">
-              <input id="email" type="text" className="validate"/>
+              <input ref="email" id="email" type="text" className="validate"/>
               <label for="email">อีเมล์</label>
             </div>
             <div className="input-field col s12 l6">
-              <input id="facebook" type="text" className="validate"/>
+              <input ref="facebook" id="facebook" type="text" className="validate"/>
               <label for="facebook">Facebook</label>
             </div>
             <div className="input-field col s12 l6">
-              <input id="line-id" type="text" className="validate"/>
+              <input ref="line-id" id="line-id" type="text" className="validate"/>
               <label for="line-id">ID Line</label>
             </div>
             <div className="input-field col s12 l6">
-              <input id="parents-name" type="text" className="validate"/>
+              <input ref="parents-name" id="parents-name" type="text" className="validate"/>
               <label for="parents-name">ชื่อผู้ปกครอง</label>
             </div>
             <div className="input-field col s12 l6">
-              <input id="relation" type="text" className="validate"/>
+              <input ref="relation" id="relation" type="text" className="validate"/>
               <label for="relation">ความสัมพันธ์</label>
             </div>
             <div className="input-field col s12 l6">
-              <input id="parents-tel" type="text" className="validate"/>
+              <input ref="parents-tel" id="parents-tel" type="text" className="validate"/>
               <label for="parents-tel">เบอร์โทรผู้ปกครอง</label>
             </div>
             <div className="input-field col s12 l12">
@@ -141,7 +141,12 @@ export default class Register extends React.Component {
             <div className="input-field col s12 l12">
               <div className="col s0 l4"><br/></div>
               <div className="col s12 l4 section-button">
-                <a className="waves-effect waves-light btn green btn-signup">ลงทะเบียนเรียน</a>
+                <button
+                    type="button"
+                    className="waves-effect waves-light btn green btn-signup"
+                >
+                   ลงทะเบียนเรียน
+                 </button>
               </div>
               <div className="col s0 l4"><br/></div>
             </div>
